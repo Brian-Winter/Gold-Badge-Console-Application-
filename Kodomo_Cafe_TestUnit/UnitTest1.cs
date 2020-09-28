8e289c62eec465ca208995b0c2a42bc86f8070a4
Brian-Winter <pawndesigns@gmail.com>
2020-09-28 18:31:46 -0400

    updated test units

--- Kodomo_Cafe_TestUnit/UnitTest1.cs
+++ Kodomo_Cafe_TestUnit/UnitTest1.cs
@@ -1,14 +1,52 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Komodo_Cafe_Repo;
 
 namespace Kodomo_Cafe_TestUnit
 {
     [TestClass]
     public class UnitTest1
     {
+        private Menu _testMenu = new Menu();
+        private List<Menu> _testMenuProperties = new List<Menu>();
+        private MenuRepository menuRepository = new MenuRepository();
         [TestMethod]
         public void TestMethod1()
         {
+            Menu thirdItem = new Menu("Hot Dog", 3, "A juicy hot dog", 1.50m);
+
+            menuRepository.AddNewMenuItem(thirdItem);
+
+
+
+           
+
+            Console.WriteLine(menuRepository.GetContentbyName("hot dog"));
+
+            Console.WriteLine(menuRepository.GetContentbyNumber(3));
+
+            Console.WriteLine(menuRepository.RemoveMenuItem("hot dog"));
+        }
+          private void ViewMenuList()
+        {
+
+            Console.Clear();
+            List<Menu> listofMenuItems = menuRepository.ViewAllMenuItems();
+
+            var sortedMenu = listofMenuItems.OrderBy(x => x.MealNumber)
+                                            .ToList();
+
+            foreach(Menu content in sortedMenu)
+            {
+                Console.WriteLine($"Meal Number: {content.MealNumber}\n" +
+                    $"Menu Name: {content.MealName}\n" +
+                    $"Meal Price: {content.MealPrice}\n" +
+                    $"Meal Description: {content.MealDescription}\n" +
+                    $"Meal Ingredients: Unknown Please add\n" +
+                    $"");
+            }
+
+
         }
     }
 }
--- Komodo_Claims_Dept_UnitTests/UnitTest1.cs
+++ Komodo_Claims_Dept_UnitTests/UnitTest1.cs
@@ -2,16 +2,29 @@
 using Komodo_Claims_Dept_Repository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+
 namespace Komodo_Claims_Dept_UnitTests
 {
     [TestClass]
     public class UnitTest1
     {
         private ClaimRepository _testClaimRepository = new ClaimRepository();
+        private Claim _testClaim = new Claim();
+
         [TestMethod]
         public void TestMethod1()
         {
+            Claim fClaim = new Claim(4, ClaimType.Car, "T-Bone Passenger Side", 3500, new DateTime(2020, 09, 21), new DateTime(2020, 09, 22), true);
 
+            _testClaimRepository.AddNewClaim(fClaim);
+
+
+            Console.WriteLine(fClaim.ClaimID);
+            Console.WriteLine(_testClaimRepository.RemoveClaim(4));
+
+            Console.WriteLine(_testClaimRepository.GetContentbyClaimId(4));
+
+            Console.WriteLine(_testClaimRepository.ViewAllClaims());
         }
     }
 }
